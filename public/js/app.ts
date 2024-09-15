de245c69cf780c6992db97e6e3de615229da7032
Rahul S <rahulsah0221@gmail.com>
2024-09-15 10:31:47 +0530

    final commit websocket app

---
+++ public/js/app.ts
@@ -0,0 +1,60 @@
+(function () {
+    let ws: WebSocket;
+
+    const messages = <HTMLElement>document.getElementById('messages');
+    const wsOpen = <HTMLButtonElement>document.getElementById('ws-open');
+    const wsClose = <HTMLButtonElement>document.getElementById('ws-close');
+    const wsSend = <HTMLButtonElement>document.getElementById('ws-send');
+    const wsInput = <HTMLInputElement>document.getElementById('ws-input');
+
+    function showMessage(message: string){
+        if(!message){
+            return;
+        }
+        messages.textContent += `\n${message}`;
+        messages.scrollTop = messages?.scrollHeight;
+    }
+
+    function closeConnection() {
+        if(!!ws) {
+            ws.close();
+        }
+    }
+
+    wsOpen.addEventListener('click', () => {
+        closeConnection()
+        ws = new WebSocket('ws://localhost:3000');
+
+        ws.addEventListener('error', () => {
+            showMessage('Websocket error')
+        })
+
+        ws.addEventListener('open', () => {
+            showMessage("Websockey connection established")
+        })
+
+        ws.addEventListener('close', () => {
+            showMessage("Websockey connection closed")
+        })
+        ws.addEventListener('message', (msg: MessageEvent<string>) => {
+            showMessage(`Received messgae: ${msg.data}`)
+        })
+    })
+
+    wsClose.addEventListener('click', closeConnection);
+    wsSend.addEventListener('click', () => {
+        const val = wsInput?.value;
+        console.log(val)
+        if(!val){
+            return;
+        }
+        else if(!ws) {
+            showMessage("No websocket connection");
+            return;
+        }
+
+        ws.send(val);
+        showMessage(`Sent "${val}`);
+        wsInput.value = '';
+    })
+})();
